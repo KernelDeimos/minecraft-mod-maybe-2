50159864643a32607483d7527a8ed6f4592178e5
KernelDeimos <eric.alex.dube@gmail.com>
2021-11-06 18:20:12 -0400

    Make Fasphalt block work

--- src/main/java/com/example/examplemod/blocks/FasphaltBlock.java
+++ src/main/java/com/example/examplemod/blocks/FasphaltBlock.java
@@ -6,8 +6,11 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.MovementInput;
 import net.minecraft.util.MovementInputFromOptions;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -25,32 +28,14 @@ public class FasphaltBlock extends Block {
 		
 	}
 
-    private static Logger logger;
-
 	@SideOnly(Side.CLIENT)
 	private static MovementInput manualInputCheck;
 
-	@SubscribeEvent
-	@SideOnly(Side.CLIENT)
-	public void speedupPlayer(PlayerTickEvent event) {
-		logger.info("I am triggering the event");
-        if (event.phase == TickEvent.Phase.START && event.side.isClient() && event.player.onGround && event.player instanceof EntityPlayerSP) {
-			if (manualInputCheck == null) {
-				manualInputCheck = new MovementInputFromOptions(Minecraft.getMinecraft().gameSettings);
-			}
-
-
-			EntityPlayerSP player = (EntityPlayerSP) event.player;
-			IBlockState blk = player.world.getBlockState(player.getPosition().down());
-			Block below = blk.getBlock();
-            if (below == this) {
-				manualInputCheck.updatePlayerMoveState();
-				if (manualInputCheck.moveForward != 0 || manualInputCheck.moveStrafe != 0) {
-					player.motionX *= 3;
-					player.motionZ *= 3;
-                    
-				}
-			}
-		}
+	@Override
+	public void onEntityWalk(World world, BlockPos pos, Entity entity) {
+		double amount = (entity.isInWater() || entity.isInLava())
+			? 1.2 : 1.4;
+		entity.motionX *= amount;
+		entity.motionY *= amount;
 	}
 }
