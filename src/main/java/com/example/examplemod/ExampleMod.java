9ba5fed9c52b77f6f777d5e5c9d0e8023d87a478
Michael Veneziano <Michael.Veneziano@turnkeygrowth.com>
2021-11-06 20:00:03 -0400

    important meta data updates

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -12,7 +12,7 @@ public class ExampleMod
 {
     public static final String MODID = "examplemod";
     public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "Over 9000";
 
     private static Logger logger;
 
