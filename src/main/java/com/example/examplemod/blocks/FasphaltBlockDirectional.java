1ed5114b02f3199ae1882df513535a8c0d2b2db0
Michael Veneziano <Michael.Veneziano@turnkeygrowth.com>
2021-11-08 18:27:27 -0500

    Directional Block

---
+++ src/main/java/com/example/examplemod/blocks/FasphaltBlockDirectional.java
@@ -0,0 +1,43 @@
+package com.example.examplemod.blocks;
+
+import com.example.examplemod.ExampleMod;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockHorizontal;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.MovementInput;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+
+@EventBusSubscriber(value = Side.CLIENT, modid = ExampleMod.MODID)
+public class FasphaltBlockDirectional extends BlockHorizontal {
+
+    
+    
+	public FasphaltBlockDirectional() {
+		super(Material.ROCK);
+		this.setHardness(3F);
+		this.setResistance(20F);
+		this.setSoundType(SoundType.STONE);
+        this.setDefaultSlipperiness(0.5f);
+	}
+
+	@SideOnly(Side.CLIENT)
+	private static MovementInput manualInputCheck;
+
+	@Override
+	public void onEntityWalk(World world, BlockPos pos, Entity entity) {
+		if (entity instanceof  EntityPlayer){
+            double amount = (entity.isInWater() || entity.isInLava())
+                ? 1.2 : 1.4;
+            entity.motionX *= amount;
+            entity.motionY *= amount;
+        }
+	}
+}
